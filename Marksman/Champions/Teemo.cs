428a4d475bd85400426e03ea2923d0b8aaf9851e
xQx <xQxCPMxQx@gmail.com>
2016-03-06 14:00:15 +0200

    Update Teemo.cs

--- Marksman/Champions/Teemo.cs
+++ Marksman/Champions/Teemo.cs
@@ -144,9 +144,9 @@ namespace Marksman.Champions
             config.AddItem(new MenuItem("UseQH" + Id, "Use Q").SetValue(false));
             config.AddItem(
                 new MenuItem("UseQTH" + Id, "Use Q (Toggle)").SetValue(new KeyBind("H".ToCharArray()[0],
-                    KeyBindType.Toggle)));
+                    KeyBindType.Toggle))).Permashow(true, "Marksman | Toggle Q");
             return true;
-        }
+            }
 
         public override bool DrawingMenu(Menu config)
         {
